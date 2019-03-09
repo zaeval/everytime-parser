0d5833b2cb8c6438f2896fbfb88e700193e6c294
zaeval <h3065@naver.com>
2019-03-10 04:12:54 +0900

    from .. remove

--- everytime_parser/examples/get_friend_list.py
+++ everytime_parser/examples/get_friend_list.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append("..")
 
-from .. import everytime
+import everytime
 
 friend_timetables = []
 ses,res = everytime.login("YOUR_ID","YOUR_PASSWORD")
--- everytime_parser/examples/send_friend.py
+++ everytime_parser/examples/send_friend.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append("..")
 
-from .. import everytime
+import everytime
 
 ses,res = everytime.login("YOUR_ID","YOUR_PASSWORD")
 print(everytime.send_friend(ses,file='alias.txt'))
